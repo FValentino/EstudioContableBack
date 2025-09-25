c128e60ed64308dc756381f59dbeb003aa473301
unknown <fariasvalentino25@gmail.com>
2025-09-25 15:49:58 -0300

    email config

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -1,4 +1,4 @@
-import { sendEmail } from "../services/email/sendEmailLanding.js";
+import { sendEmail, sendEmailResend } from "../services/email/sendEmailLanding.js";
 
 
 export async function sendContactEmail(req, res) {
@@ -9,7 +9,7 @@ export async function sendContactEmail(req, res) {
   }
 
   try {
-    await sendEmail(subject, html);
+    await sendEmailResend(subject, html);
     res.status(200).json({ success: true, message: "Email enviado correctamente!" });
   } catch (err) {
     res.status(500).json({ success: false, message: "Error al enviar el email" });
