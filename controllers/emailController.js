79c36ae3ee5f5e823f25424820eacbe17006cfdf
unknown <fariasvalentino25@gmail.com>
2025-09-25 22:41:27 -0300

    news email

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -16,16 +16,14 @@ export async function sendContactEmail(req, res) {
 }
 
 export async function sendNewsEmail(req, res) {
-  const { subject, email} = req.body;
+  const { subject, text} = req.body;
 
   if (!subject || !email ) {
     return res.status(400).json({ success: false, message: "Todos los campos son obligatorios" });
   }
 
-  const textMessage = `Me interesa recibir emails ocn novedades:\nMi emailes: ${email}`;
-
   try {
-    await sendEmail(subject, textMessage);
+    await sendEmail(subject, text);
     res.status(200).json({ success: true, message: "Email enviado correctamente!" });
   } catch (err) {
     res.status(500).json({ success: false, message: "Error al enviar el email" });
--- services/email/sendEmailLanding.js
+++ services/email/sendEmailLanding.js
@@ -5,7 +5,7 @@ import { Resend } from 'resend';
 
 dotenv.config();
 
-export async function sendEmailResend(subject, text) {
+export async function sendEmail(subject, text) {
   try {
     const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -16,12 +16,6 @@ export async function sendEmailResend(subject, text) {
       html: text
     });
 
-    console.log("EMAIL FROM: ", process.env.FROM_EMAIL)
-    console.log("EMAIL TO: ", process.env.USER_EMAIL)
-    console.log("SUJETO: [web] ",subject)
-    console.log("HTML: ", text)
-
-    console.log("Email enviado:", data);
     return data;
   } catch (error) {
     console.error("Error enviando email:", error);
@@ -29,20 +23,4 @@ export async function sendEmailResend(subject, text) {
   }
 }
 
-export async function sendEmail(subject, html) {
-  try {
-    const info = await transporter.sendMail({
-      from: "I&M <onboarding@resend.dev>", // remitente válido de Resend
-      to: process.env.USER_EMAIL,          // tu Gmail
-      subject: `[web] ${subject}`,
-      html: html,
-    });
-
-    console.log("✅ Email enviado:", info.messageId);
-    return info;
-  } catch (error) {
-    console.error("❌ Error al enviar email:", error);
-    throw error;
-  }
-}
 
